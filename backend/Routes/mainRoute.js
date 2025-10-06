0b383a0054f0ea177fa8a7f59e61ea528dec6cef
Ayush Auti <ayushauti5605@gmail.com>
2025-10-06 20:39:55 +0530

    issue router added

--- backend/Routes/mainRoute.js
+++ backend/Routes/mainRoute.js
@@ -1,12 +1,14 @@
 import express from "express";
 import {userRouter} from "./userRouter.js";
 import { repoRouter } from "./repoRoute.js";
+import { issueRouter } from "./issueRoute.js";
 
 
 const mainRouter=express.Router();
 
 mainRouter.use(userRouter);
 mainRouter.use(repoRouter);
+mainRouter.use(issueRouter);
 
  mainRouter.get("/",(req,res)=>{
             res.send("welcome");
---
+++ backend/controllers/issueController.js
@@ -0,0 +1,19 @@
+export const createIssue=()=>{
+    res.send("issue created");
+};
+
+export const updateIssueById=()=>{
+    res.send("issue updated");
+};
+
+export const deleteIssueById=()=>{
+    res.send("issue deleted");
+};
+
+export const getAllIssue=()=>{
+    res.send("fetching issues");
+};
+
+export const getIssueById=()=>{
+    res.send("fetch issue by id");
+};
