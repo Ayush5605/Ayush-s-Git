706f3b1c7df72d41163117ec7192600ee5583b76
Ayush Auti <ayushauti5605@gmail.com>
2025-11-01 22:59:38 +0530

    fetching repo to frontend added

--- frontend/src/components/dashboard/Dashboard.jsx
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -1,6 +1,50 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 
 const Dashboard=()=>{
+
+    const [repositiories,setRepositiories]=useState([]);
+    const [searchQuery,setSearchQuery]=useState('');
+    const[suggestedRepositiories,setSuggestedRepositiories]=useState([]);
+    const[searchResult,setSearchResult]=useState([]);
+
+    useEffect(()=>{
+
+        const userId=localStorage.getItem("userId");
+
+        const fetchRepositiories=async()=>{
+
+            try{
+                    const response=await fetch(`http://localhost:3000/repo/user/${userId}`);
+
+                    const data=await response.json();
+                    setRepositiories(data);
+            }catch(err){
+                console.log(err);
+            }
+
+
+        };
+         const fetchSuggestedRepositiories=async()=>{
+
+            try{
+                    const response=await fetch(`http://localhost:3000/repo/all`);
+
+                    const data=await response.json();
+                    setSuggestedRepositiories(data);
+                    console.log(suggestedRepositiories);
+            }catch(err){
+                console.log(err);
+            }
+                    
+
+
+        };
+        fetchRepositiories();
+        fetchSuggestedRepositiories();
+
+
+
+    },[]);
     return(
     <h1>Dashboard</h1>
     )
